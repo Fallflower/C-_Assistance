43ebd3a12ef3f852e6334d370222c6d4363dbceb
Fallflower <xy673636000@gmail.com>
2024-03-13 10:20:47 +0800

    Update class Error

--- Assistance.h
+++ Assistance.h
@@ -5,6 +5,7 @@
 
 // ANSI C++标准库头文件
 #include <cstring>					// 标准串操作
+#include <string>					// c++ string
 #include <iostream>					// 标准流操作
 #include <limits>					// 极限
 #include <cmath>					// 数据函数
@@ -16,6 +17,7 @@
 #include <iomanip>					// 输入输出流格式设置	
 #include <cstdarg> 					// 支持变长函数参数	
 #include <cassert>					// 支持断言
+#include <exception>				// 异常处理
 using namespace std;				// 标准库包含在命名空间std中
 
 // 自定义类型
@@ -40,9 +42,6 @@ void Display(ElemType elem[], int n);	// 显示数组elem的各数据元素值
 template <class ElemType>
 void Write(const ElemType &e);			// 显示数据元素
 
-// 辅助类
-class Error;			// 通用异常类
-
 char GetChar(istream &inStream)
 // 操作结果：从输入流inStream中跳过空格及制表符获取一字符
 {
@@ -57,33 +56,23 @@ char GetChar(istream &inStream)
 }
 
 // 通用异常类                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                    
-#define MAX_ERROR_MESSAGE_LEN 100
-class Error
+
+class Error : public exception
 {
 private:
 // 数据成员
-	char message[MAX_ERROR_MESSAGE_LEN];// 异常信息
+	unsigned int ecode;  // 错误代码
+	string mes;			// 异常信息
 
 public:
 //  方法声明
-	Error(const char *mes = "一般性异常!");	// 构造函数 
-	~Error(void) {};					// 析构函数	
-	void Show() const;					// 显示异常信息
+	Error(unsigned int code, const string& message):ecode(code), mes(message){};
+	virtual const char * what(void) const noexcept override
+	{
+		string text = "Error code:\t" + to_string(ecode) + "\nMessage:\t" + mes;
+		return text.c_str();
+	}
 };
-
-// 通用异常类的实现部分
-Error::Error(const char *mes)
-// 操作结果：由mes构构通用异常对象
-{
-	strcpy(message, mes);				// 复制异常信息
-}
-
-void Error::Show()const
-// 操作结果：显示异常信息
-{
-	cout << message << endl;			// 显示异常信息	
-}
-
 
 template <class ElemType >
 void Swap(ElemType &e1, ElemType &e2)
